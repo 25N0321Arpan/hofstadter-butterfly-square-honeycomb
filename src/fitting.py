e653f53bf4e1e83dd3bbd5c6f5065160cff68c98
25N0321Arpan <25N0321@iitb.ac.in>
2026-06-01 14:11:07 +0530

    Implement power law fitting in fitting.py
    Added a power law fitting function and applied it to data.

--- src/fitting.py
+++ src/fitting.py
@@ -1,1 +1,9 @@
+def power_law(x, A, eta):
+    return A * x**eta
 
+x_fit = x_full[1:]
+E_fit_data = Energy_greaterzero[1:]
+popt, pcov = curve_fit(power_law, x_fit, E_fit_data, p0=[E_fit_data[0], 1.0])
+A, eta = popt
+print("A =", A)
+print("eta =", eta)
