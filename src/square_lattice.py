c4fcb0ea75bc29cd8ed0b980043f37409a182a80
25N0321Arpan <25N0321@iitb.ac.in>
2026-06-01 14:12:33 +0530

    Add harper_matrix function to create Hamiltonian matrix

--- src/square_lattice.py
+++ src/square_lattice.py
@@ -1,1 +1,11 @@
+def harper_matrix(N, alpha, ky=0.0, t=1.0):
+    H = np.zeros((N, N), dtype=float)
 
+    for m in range(N):
+        H[m, m] = -2 * t * np.cos(ky + 2 * np.pi * alpha * m)
+
+        if m < N-1 :
+            H[m, m + 1] = -t
+            H[m + 1, m] = -t
+
+    return H
